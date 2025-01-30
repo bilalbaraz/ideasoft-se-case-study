35098c3ed13c0542e60d9a2f5857c6e6d482abf1
Bilal Baraz <bilalbaraz@windowslive.com>
2025-01-30 13:41:01 +0300

    Create unit test classes

---
+++ tests/Unit/Api/V1/Controllers/Order/DiscountControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Api\V1\Controllers\Order;
+
+use PHPUnit\Framework\TestCase;
+
+class DiscountControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
